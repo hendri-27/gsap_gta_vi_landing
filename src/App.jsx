0cfdccf617e663a1d9f05e4053670a786c7dea7c
hendri-27 <62458850+hendri-27@users.noreply.github.com>
2025-09-18 14:36:29 +0700

    Implement Post Card

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import FirstVideo from './sections/FirstVideo';
 import Jason from './sections/Jason';
 import SecondVideo from './sections/SecondVideo';
 import Lucia from './sections/Lucia';
+import PostCard from './sections/PostCard';
 
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(GSDevTools);
@@ -24,6 +25,8 @@ const App = () => {
       <SecondVideo />
       {/* <hr className='fixed top-[5%] w-full h-1 bg-amber-300 z-50' /> */}
       <Lucia />
+
+      <PostCard />
     </main>
   )
 }
